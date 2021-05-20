9802b1c08272eb6f0045632b954a703637016bc5
robertgrc <robert.rodriguez.rgrc@gmail.com>
2021-05-20 10:55:25 -0400

    Add Component Header

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import Header from './components/Header';
 
 function App() {
   return (
-   <h1>Cotizador</h1>
+   <Header 
+     titulo="Cotizador de Seguros"
+   />
   );
 }
 
