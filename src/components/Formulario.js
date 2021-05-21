6193d7e7dfd448b7ed27b04cbfbb280a5aeaf3c8
robertgrc <robert.rodriguez.rgrc@gmail.com>
2021-05-20 22:41:04 -0400

    finalizing the styles of the form component

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -23,6 +23,27 @@ const InputRadio = styled.input`
     margin: 0 1rem;
 `
 
+const Boton = styled.button`
+    background-color: #00838F;
+    font-size: 16px;
+    width: 100%;
+    padding: 1rem;
+    color: #fff;
+    text-transform: uppercase;
+    font-weight: bold;
+    border: none;
+    transition: background-color .3s ease;
+    margin-top: 2rem;
+
+    /* Configurar el hover el cursor */
+    &:hover {
+        cursor: pointer;
+        background-color: #26C6DA;
+    }
+`;
+
+
+
 const Formulario = () => {
     return (  
         <form>
@@ -67,7 +88,7 @@ const Formulario = () => {
                     /> Completo
             </Campo>
 
-            <button type="button">Cotizar</button>
+            <Boton type="button">Cotizar</Boton>
         </form>
     );
 }
